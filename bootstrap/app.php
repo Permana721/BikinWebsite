e65ead16c128513858aef4c7721529ee6a5dc8aa
Permana <sdharma721@gmail.com>
2026-04-29 11:54:26 +0700

    refactor(bootstrap): update user redirection in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->redirectUsersTo(function () {
+            return url()->previous();
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
