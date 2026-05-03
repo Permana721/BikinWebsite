3f12fdabc38b27bbabf5b3e006200e2a517d6ef9
Permana <sdharma721@gmail.com>
2026-05-03 17:57:00 +0700

    feat(layout): update title, add favicon (resources/views/user/layouts/app.blade.php)

--- resources/views/user/layouts/app.blade.php
+++ resources/views/user/layouts/app.blade.php
@@ -5,7 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Dasbor Saya') | WEB-UKM</title>
+    <title>@yield('title', 'Dasbor Saya') | BikinSitus</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo/BikinSitusLogo.png') }}">
     
     <script src="https://cdn.tailwindcss.com"></script>
     
