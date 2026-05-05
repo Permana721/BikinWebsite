1670c1c14fc05a2e14b34230ff0696786337f998
Permana <sdharma721@gmail.com>
2026-05-05 08:50:21 +0700

    refactor(user-profile): update site name in profile title

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil Saya | BikinSitus</title>
+    <title>Profil Saya | BikinWebsite</title>
     
     <script src="https://cdn.tailwindcss.com"></script>
     
