9244b278c65cf40cba48aa2173ed961e523da171
Permana <sdharma721@gmail.com>
2026-05-05 08:49:54 +0700

    feat(layouts): update title and favicon for admin dashboard (BikinWebsite)

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Dasbor Admin') | BikinSitus</title>
-    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo/BikinSitusAdminLogo.png') }}">
+    <title>@yield('title', 'Dasbor Admin') | BikinWebsite</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/logo/BikinWebsiteAdminLogo.png') }}">
     
     <script src="https://cdn.tailwindcss.com"></script>
     
